80eeb73d8aaf4337394b7ee7054464064c5466ae
vskeerthi0234 <vskeerthi1729@gmail.com>
2024-06-12 12:45:29 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,19 +1,2 @@
-'''n=int(input("enter"))
-for i in range(n+1):
-    print(" * ")
-    for j in range(i+1):
-        print(" * ",end="")'''
-co=input("enter the cordinates")
-n=int(input("number"))
-if((co=='a')or(co=='c')or(co=='e')or(co=='g')):
-    if(n%2==0):
-        print("white")
-    else:
-        print("black")
-else:
-    if(n%2==0):
-        print("black")
-    else:
-        print("white")
-        
+
 
